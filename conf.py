f4e3e02de7d84e0bbc5910ccec7b6f3c5223912e
Tikumporn Wankvar <secondstagedev@gmail.com>
2021-01-20 00:53:18 +0700

    change theme to classic

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@ import sys
 #import django
 sys.path.insert(0, os.path.abspath('..'))
 #os.environ['DJANGO_SETTINGS_MODULE'] = 'Salary_Project.settings'
-#django.setup()
+# django.setup()
 
 
 # -- Project information -----------------------------------------------------
@@ -57,7 +57,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
